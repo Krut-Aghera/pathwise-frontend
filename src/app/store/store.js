466c5bf27cf522f33b232aeef2ceb979518e1473
Krut-Aghera <krut.aghera195@gmail.com>
2026-08-31 18:37:14 +0530

    remove lecture feature mess

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit"
 
 import authReducer from "../../features/auth/authSlice"
+
 import authApi from "../../features/auth/authApi"
 import userApi from "../../features/user/userApi"
 import courseApi from "../../features/course/courseApi"
